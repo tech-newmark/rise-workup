8d56b675b31f0c36bbcc157a33d1d1180338f5e5
Yuriy Bevov <yuriybevov@gmail.com>
2026-06-29 12:38:12 +0300

    правки перед выгрузкой

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,119 +1,119 @@
 <?php
-$arUrlRewrite = array(
-  1 =>
-  array(
+$arUrlRewrite=array (
+  1 => 
+  array (
     'CONDITION' => '#^\\/?\\/mobileapp/jn\\/(.*)\\/.*#',
     'RULE' => 'componentName=$1',
     'ID' => NULL,
     'PATH' => '/bitrix/services/mobileapp/jn.php',
     'SORT' => 100,
   ),
-  3 =>
-  array(
+  3 => 
+  array (
     'CONDITION' => '#^/bitrix/services/ymarket/#',
     'RULE' => '',
     'ID' => '',
     'PATH' => '/bitrix/services/ymarket/index.php',
     'SORT' => 100,
   ),
-  0 =>
-  array(
+  0 => 
+  array (
     'CONDITION' => '#^/stssync/calendar/#',
     'RULE' => '',
     'ID' => 'bitrix:stssync.server',
     'PATH' => '/bitrix/services/stssync/calendar/index.php',
     'SORT' => 100,
   ),
-  28 =>
-  array(
+  28 => 
+  array (
     'CONDITION' => '#^/company/articles/#',
     'RULE' => '',
     'ID' => 'bitrix:news',
     'PATH' => '/company/articles/index.php',
     'SORT' => 100,
   ),
-  29 =>
-  array(
+  29 => 
+  array (
     'CONDITION' => '#^/optovym-klientam/#',
     'RULE' => '',
     'ID' => 'bitrix:news',
     'PATH' => '/optovym-klientam/index.php',
     'SORT' => 100,
   ),
-  6 =>
-  array(
+  6 => 
+  array (
     'CONDITION' => '#^/personal/order/#',
     'RULE' => '',
     'ID' => 'bitrix:sale.personal.order',
     'PATH' => '/personal/order/index.php',
     'SORT' => 100,
   ),
-  26 =>
-  array(
+  26 => 
+  array (
     'CONDITION' => '#^/customization/#',
     'RULE' => '',
     'ID' => 'bitrix:news',
     'PATH' => '/customization/index.php',
     'SORT' => 100,
   ),
-  25 =>
-  array(
-    'CONDITION' => '#^/personal/#',
-    'RULE' => '',
-    'ID' => 'bitrix:sale.personal.section',
-    'PATH' => '/personal/index.php',
-    'SORT' => 100,
-  ),
-  32 =>
-  array(
-    'CONDITION' => '#^/services/#',
-    'RULE' => '',
-    'ID' => 'bitrix:news',
-    'PATH' => '/services/index.php',
-    'SORT' => 100,
-  ),
-  34 =>
-  array(
+  34 => 
+  array (
     'CONDITION' => '#^/articles/#',
     'RULE' => '',
     'ID' => 'bitrix:news',
     'PATH' => '/articles/index.php',
     'SORT' => 100,
   ),
-  35 =>
-  array(
+  36 => 
+  array (
+    'CONDITION' => '#^/services/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/services/index.php',
+    'SORT' => 100,
+  ),
+  37 => 
+  array (
+    'CONDITION' => '#^/personal/#',
+    'RULE' => '',
+    'ID' => 'bitrix:sale.personal.section',
+    'PATH' => '/personal/index.php',
+    'SORT' => 100,
+  ),
+  35 => 
+  array (
     'CONDITION' => '#^/catalog/#',
     'RULE' => '',
     'ID' => 'bitrix:catalog',
     'PATH' => '/catalog/index.php',
     'SORT' => 100,
   ),
-  8 =>
-  array(
+  8 => 
+  array (
     'CONDITION' => '#^/store/#',
     'RULE' => '',
     'ID' => 'bitrix:catalog.store',
     'PATH' => '/store/index.php',
     'SORT' => 100,
   ),
-  33 =>
-  array(
+  33 => 
+  array (
     'CONDITION' => '#^/sales/#',
     'RULE' => '',
     'ID' => 'bitrix:news',
     'PATH' => '/sales/index.php',
     'SORT' => 100,
   ),
-  2 =>
-  array(
+  2 => 
+  array (
     'CONDITION' => '#^/rest/#',
     'RULE' => '',
     'ID' => NULL,
     'PATH' => '/bitrix/services/rest/index.php',
     'SORT' => 100,
   ),
-  12 =>
-  array(
+  12 => 
+  array (
     'CONDITION' => '#^/news/#',
     'RULE' => '',
     'ID' => 'bitrix:news',
