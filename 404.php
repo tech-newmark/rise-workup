7416decd439dd6def2ef2365caa1870602a195e1
OkTarasova <118619788+OkTarasova@users.noreply.github.com>
2026-07-10 22:22:15 +0300

    Правлю хлебные крошки на 404

--- 404.php
+++ 404.php
@@ -9,45 +9,46 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 
 /** @global CMain $APPLICATION */
 
-$APPLICATION->SetTitle("Страница не найдена"); ?>
+$APPLICATION->SetTitle("Страница не найдена");
+$APPLICATION->SetPageProperty("NOT_SHOW_NAV_CHAIN", "Y"); ?>
 
 <section class="base-section nfp" style="
     padding: 100px 20px;
     color: #333;
     text-align: center;
 ">
-	<div class="container" style="
+    <div class="container" style="
         max-width: 1100px;
         margin: 0 auto;
     ">
-		<div class="nfp__wrapper content" style="
+        <div class="nfp__wrapper content" style="
             display: flex;
             flex-direction: column;
             align-items: center;
             gap: 30px;
         ">
-			<h1 style="
+            <h1 style="
                 color: var(--primary);
                 font-size: clamp(84px, 16vw, 280px);
                 margin: 0;
                 font-weight: bold;
             ">404</h1>
 
-			<p style="
+            <p style="
                 font-size: clamp(16px, 2vw, 20px);
                 line-height: 1.6;
                 max-width: 600px;
 				margin: 0;
             ">К сожалению, такой страницы не существует или она была удалена...</p>
 
-			<a href="/" style="
+            <a href="/" style="
                 color: var(--primary);
                 font-size: 1rem;
                 text-decoration: underline;">
-				Вернуться на главную
-			</a>
-		</div>
-	</div>
+                Вернуться на главную
+            </a>
+        </div>
+    </div>
 </section>
 <?php
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
