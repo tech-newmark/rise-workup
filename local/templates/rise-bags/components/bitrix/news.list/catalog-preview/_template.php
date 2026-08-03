05e5ec3c1636f65a4cb62794db49f5e01e45c311
Yuriy Bevov <yuriybevov@gmail.com>
2026-08-03 11:12:51 +0300

    Правка стилей и удаление лишних файлов

--- local/templates/rise-bags/components/bitrix/news.list/catalog-preview/_template.php
+++
@@ -1,7 +0,0 @@
-<? 
-		if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-		$this->setFrameMode(true);
-
-		// includeComponentAssets('news.list/catalog-preview');
-	?>
-	<?/* debug($arResult) */?>
