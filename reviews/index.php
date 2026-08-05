dbd44e738467f1763569378660032dcec644b8fb
OkTarasova <118619788+OkTarasova@users.noreply.github.com>
2026-08-05 16:45:59 +0300

    Добавляю компонент с отзывами

---
+++ reviews/index.php
@@ -0,0 +1,11 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Отзывы");
+?>
+
+<section>
+	<h2>Благодарственные письма</h2>
+	<p>Наши клиенты нам благодарны.</p>
+</section>
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
