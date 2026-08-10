dc1da20a52edac4393b27ca87b06ea18bc864718
OkTarasova <118619788+OkTarasova@users.noreply.github.com>
2026-08-10 13:50:28 +0300

    Правка компонента Галереи

---
+++ local/templates/rise-bags/components/bitrix/news.list/gallery-slider/.parameters.php
@@ -0,0 +1,43 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$arTemplateParameters = array(
+	"DISPLAY_DATE" => array(
+		"NAME" => GetMessage("T_IBLOCK_DESC_NEWS_DATE"),
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "Y",
+	),
+	"DISPLAY_NAME" => array(
+		"NAME" => GetMessage("T_IBLOCK_DESC_NEWS_NAME"),
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "Y",
+	),
+	"DISPLAY_PICTURE" => array(
+		"NAME" => GetMessage("T_IBLOCK_DESC_NEWS_PICTURE"),
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "Y",
+	),
+	"DISPLAY_PREVIEW_TEXT" => array(
+		"NAME" => GetMessage("T_IBLOCK_DESC_NEWS_TEXT"),
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "Y",
+	),
+	"SECTION_GALLERY" => array(
+		"PARENT" => "VISUAL",
+		"NAME" => "Выводить галерею как секцию",
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "N",
+	),
+	"SECTION_TITLE" => array(
+		"PARENT" => "VISUAL",
+		"NAME" => "Заголовок секции",
+		"TYPE" => "STRING",
+		"DEFAULT" => "",
+	),
+	"SECTION_DESCRIPTION" => array(
+		"PARENT" => "VISUAL",
+		"NAME" => "Описание секции",
+		"TYPE" => "STRING",
+		"DEFAULT" => "",
+	),
+);
