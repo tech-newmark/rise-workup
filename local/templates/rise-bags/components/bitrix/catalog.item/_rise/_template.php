fc40539195b055748c989895290e715af85e9cc7
Yuriy Bevov <yuriybevov@gmail.com>
2026-08-26 15:17:45 +0300

    удалил тестовый шаблон

--- local/templates/rise-bags/components/bitrix/catalog.item/_rise/_template.php
+++
@@ -1,7 +0,0 @@
-<? 
-		if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-		$this->setFrameMode(true);
-
-		// includeComponentAssets('catalog.item/_rise');
-	?>
-	<?/* debug($arResult) */?>
