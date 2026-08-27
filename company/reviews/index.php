6ebbec67896156d22e038171a3778fcbc1b7a842
OkTarasova <118619788+OkTarasova@users.noreply.github.com>
2026-08-27 14:56:40 +0300

    Подтягиваем правки от сео

--- company/reviews/index.php
+++ company/reviews/index.php
@@ -1,6 +1,8 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
-$APPLICATION->SetTitle("Отзывы");
+$APPLICATION->SetPageProperty("description", "Отзывы клиентов о компании RISE: пошив сумок, рюкзаков, папок, шопперов и текстильной продукции на заказ. Благодарственные письма, оценки на внешних площадках и примеры выполненных заказов.");
+$APPLICATION->SetPageProperty("title", "Отзывы и благодарности клиентов RISE — пошив сумок и текстильной продукции");
+$APPLICATION->SetTitle("Отзывы и благодарности клиентов RISE");
 
 $request = \Bitrix\Main\Context::getCurrent()->getRequest();
 $requestedYear = (string)$request->getQuery('year');
@@ -148,81 +150,81 @@ $reviewsBaseUrl = $APPLICATION->GetCurPage();
 
 
 		<? $APPLICATION->IncludeComponent(
-			"bitrix:news.list",
-			"reviews-list",
-			[
-				"ACTIVE_DATE_FORMAT" => "j F Y",
-				"ADD_SECTIONS_CHAIN" => "N",
-				"AJAX_MODE" => "N",
-				"AJAX_OPTION_ADDITIONAL" => "",
-				"AJAX_OPTION_HISTORY" => "N",
-				"AJAX_OPTION_JUMP" => "N",
-				"AJAX_OPTION_STYLE" => "Y",
-				"CACHE_FILTER" => "Y",
-				"CACHE_GROUPS" => "Y",
-				"CACHE_TIME" => "36000000",
-				"CACHE_TYPE" => "A",
-				"CHECK_DATES" => "Y",
-				"COMPONENT_TEMPLATE" => "reviews-list",
-				"DETAIL_URL" => "",
-				"DISPLAY_BOTTOM_PAGER" => "Y",
-				"DISPLAY_DATE" => "Y",
-				"DISPLAY_NAME" => "Y",
-				"DISPLAY_PICTURE" => "Y",
-				"DISPLAY_PREVIEW_TEXT" => "Y",
-				"DISPLAY_TOP_PAGER" => "N",
-				"FIELD_CODE" => [
-					0 => "DATE_ACTIVE_FROM",
-					1 => "",
-				],
-				"FILTER_NAME" => "reviewsFilter",
-				"SELECTED_YEAR" => $selectedReviewYear,
-				"REVIEWS_YEARS" => $reviewYears,
-				"REVIEWS_BASE_URL" => $reviewsBaseUrl,
-				"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-				"IBLOCK_ID" => "21",
-				"IBLOCK_TYPE" => "site_content",
-				"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-				"INCLUDE_SUBSECTIONS" => "Y",
-				"MESSAGE_404" => "",
-				"NEWS_COUNT" => "4",
-				"PAGER_BASE_LINK_ENABLE" => "N",
-				"PAGER_DESC_NUMBERING" => "N",
-				"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-				"PAGER_SHOW_ALL" => "N",
-				"PAGER_SHOW_ALWAYS" => "N",
-				"PAGER_TEMPLATE" => "round",
-				"PAGER_TITLE" => "Новости",
-				"PARENT_SECTION" => "",
-				"PARENT_SECTION_CODE" => "",
-				"PREVIEW_TRUNCATE_LEN" => "",
-				"PROPERTY_CODE" => [
-					0 => "RATING",
-					1 => "",
-				],
-				"SET_BROWSER_TITLE" => "N",
-				"SET_LAST_MODIFIED" => "N",
-				"SET_META_DESCRIPTION" => "N",
-				"SET_META_KEYWORDS" => "N",
-				"SET_STATUS_404" => "N",
-				"SET_TITLE" => "N",
-				"SHOW_404" => "N",
-				"SORT_BY1" => "ACTIVE_FROM",
-				"SORT_BY2" => "SORT",
-				"SORT_ORDER1" => "ASC",
-				"SORT_ORDER2" => "ASC",
-				"STRICT_SECTION_CHECK" => "N"
-			],
-			false
-		); ?>
+	"bitrix:news.list", 
+	"reviews-list", 
+	[
+		"ACTIVE_DATE_FORMAT" => "j F Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "Y",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"COMPONENT_TEMPLATE" => "reviews-list",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => [
+			0 => "DATE_ACTIVE_FROM",
+			1 => "",
+		],
+		"FILTER_NAME" => "reviewsFilter",
+		"SELECTED_YEAR" => $selectedReviewYear,
+		"REVIEWS_YEARS" => $reviewYears,
+		"REVIEWS_BASE_URL" => $reviewsBaseUrl,
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "21",
+		"IBLOCK_TYPE" => "site_content",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "12",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => "round",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => [
+			0 => "RATING",
+			1 => "",
+		],
+		"SET_BROWSER_TITLE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N"
+	],
+	false
+); ?>
 	</div>
 </section>
 <!-- Отзывы -->
 
 <!-- Благодарности -->
 <? $APPLICATION->IncludeComponent(
-	"bitrix:news.list",
-	"gallery-slider",
+	"bitrix:news.list", 
+	"gallery-slider", 
 	[
 		"ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"ADD_SECTIONS_CHAIN" => "N",
@@ -284,8 +286,9 @@ $reviewsBaseUrl = $APPLICATION->GetCurPage();
 		"STRICT_SECTION_CHECK" => "N",
 		"USE_FANCY" => "Y",
 		"SECTION_TITLE" => "Благодарственные письма",
-		"SECTION_DESCRIPTION" => "Наши клиенты нам благодарны.",
-		"SECTION_GALLERY" => "Y"
+		"SECTION_DESCRIPTION" => "Часть заказчиков направляет официальные благодарственные письма после завершения проектов. Мы размещаем их как подтверждение качества пошива, соблюдения сроков и внимательного отношения к требованиям по материалам, конструкции и брендированию изделий.",
+		"SECTION_GALLERY" => "Y",
+		"SHOW_FOOTER" => "N"
 	],
 	false
 ); ?>
@@ -293,8 +296,8 @@ $reviewsBaseUrl = $APPLICATION->GetCurPage();
 
 <!-- Клиенты -->
 <? $APPLICATION->IncludeComponent(
-	"bitrix:news.list",
-	"gallery-slider",
+	"bitrix:news.list", 
+	"gallery-slider", 
 	[
 		"ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"ADD_SECTIONS_CHAIN" => "N",
@@ -356,7 +359,7 @@ $reviewsBaseUrl = $APPLICATION->GetCurPage();
 		"SORT_ORDER2" => "ASC",
 		"STRICT_SECTION_CHECK" => "N",
 		"SECTION_TITLE" => "Нам доверяют компании из разных сфер",
-		"SECTION_DESCRIPTION" => "",
+		"SECTION_DESCRIPTION" => "RISE работает с розничными магазинами, корпоративными заказчиками, event-агентствами, образовательными проектами, производственными компаниями и организациями, которым нужна текстильная продукция для сотрудников, клиентов, партнеров или мероприятий.",
 		"SECTION_GALLERY" => "Y",
 		"SHOW_FOOTER" => "N",
 		"USE_FANCY" => "N"
